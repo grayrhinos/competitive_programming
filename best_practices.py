c6efd29fd59a19c2af432a764c60e439520fa5f3
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2020-08-04 19:59:17 +0900

    Create best_practices.py

---
+++ best_practices.py
@@ -0,0 +1,7 @@
+# Relatively fast way to read line in CodeForces
+
+for i in range(int(input())):
+    n = int(input())
+    num = list(map(int, input().split()))
+    
+    for i in range(len(num)):
