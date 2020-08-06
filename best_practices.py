37d9e6ecc7754d6e1eacb936db6a64953c7d122d
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2020-08-06 18:29:03 +0900

    Update best_practices.py

--- best_practices.py
+++ best_practices.py
@@ -1,7 +1,43 @@
 # Relatively fast way to read line in CodeForces
 
-for i in range(int(input())):
-    n = int(input())
-    num = list(map(int, input().split()))
+# Tip #1
+'''
+- Use alias for the functions that are called too many times.
+* 'Local variable lookups are much faster than global or built-in variable lookups: the Python "compiler" optimizes most function bodies so that for local variables, 
+no dictionary lookup is necessary, but a simple array indexing operation is sufficient.'
+
+* Local variables are faster than global; if you use global constants in a loop, copy it to a local variable beofre the loop. In Pyhton function names (global or build-in) 
+are also global constants
+
+* Try to use map(), filter() or reduce() to replace an explicit for loop, but only if you can use a built-in function: map with a built-in function beats for loop, 
+but a for loop with in-line code beats map with a lambda function!
+- For example, if we want to convert each string in a list to upper/lower case we can do that efficiently as new_list = map(str.upper, old_list)
+
+- In most of the cases the number of input is huge. That means we have to call input() function many times to read each line. So, every time we call input() function,
+Python recognizes that input() is a build in function and looks up for that does input() do. After lookup is when it starts executing it. By using alias 
+(assigning a new name to input()) we can save that lookup time. 
+'''
+xyz = input
+t = int(xyz()) 
+for i1 in [0]*t:
+    n = int(xyz())
+    num = list(map(int, xyz().split()))
     
     for i in range(len(num)):
+
+
+        
+        
+        
+        
+# Code for testing runtime
+import timeit
+
+print(timeit.timeit(
+'''
+
+j = 0
+for i in [0]*100000:
+    j += 1
+'''    
+, number = 1000))
